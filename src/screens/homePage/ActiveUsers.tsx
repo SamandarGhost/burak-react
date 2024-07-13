98ae722cef6d35f3128e3aea5080fae368e187fd
Sam <kimjeyeon03@icloud.com>
2024-07-13 17:57:04 +0900

    feat: active users sectional components

--- src/screens/homePage/ActiveUsers.tsx
+++ src/screens/homePage/ActiveUsers.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import { CssVarsProvider } from '@mui/joy';
+
+const activeUsers = [
+    {memberNick: "Martin", memberImage: "/img/martin.webp" },
+    {memberNick: "Justin", memberImage: "/img/justin.webp" },
+    {memberNick: "Rose", memberImage: "/img/rose.webp" },
+    {memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
-    return (<div>
+    return (
+    <div className={"active-users-frame"}>
         <Container>
-            ActiveUsers
+            <Stack className={"main"}>
+                <Box className={"category-title"}>Active Users</Box>
+                <Stack className={"cards-frame"}>
+                    <CssVarsProvider>
+                        {activeUsers.length !== 0 ? (
+                            activeUsers.map((ele, index) => {
+                                return (
+                                    <Stack className={"card"}>
+                                        <img src={ele.memberImage} />
+                                        <div className={"card-name"}>{ele.memberNick}</div>
+                                    </Stack>
+                                );
+                            }) ) : (<Box className={"no-data"}> No Active Users!</Box>)}
+                    </CssVarsProvider>
+                </Stack>
+            </Stack>
         </Container>
-    </div>);
+    </div>
+    );
 }
