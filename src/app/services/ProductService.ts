649e8a4ad59a3fbecaaa44cbb1d861057de9e40f
Sam <kimjeyeon03@icloud.com>
2024-08-08 19:09:38 +0900

    fix: modify choosenProduct component logic

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -16,7 +16,19 @@ class ProductService {
             console.log("getProducts", result);
             return result.data;
         }catch(err){
-            console.log("Error, getProduct ",err);
+            console.log("Error, getProducts: ",err);
+            throw err;
+        }
+    }
+
+    public async getProduct(productId: string): Promise<Product> {
+        try {
+            const url = `${this.path}/product/${productId}`;
+            const result = await axios.get(url, {withCredentials: true});
+
+            return result.data;
+        } catch(err) {
+            console.log("Error, getProduct:", err);
             throw err;
         }
     }
--- src/screens/productsPage/ChosenProduct.tsx
+++ src/screens/productsPage/ChosenProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Stack, Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
@@ -16,11 +16,16 @@ import { retrieveChoosenProduct, retrieveRestaurant } from "./selector";
 import { createSelector, Dispatch } from "@reduxjs/toolkit";
 import { Product } from "../../lib/types/product";
 import { Member } from "../../lib/types/member";
+import { useParams } from "react-router-dom";
+import ProductService from "../../app/services/ProductService";
+import MemberService from "../../app/services/MemeberService";
+import { useDispatch, useSelector } from "react-redux";
+import { serverApi } from "../../lib/config";
 
 /* Redux Slice and Selector */
 const actionDispatch = (dispatch: Dispatch) => ({
-  setRestaurant: (data: Member[]) => dispatch(setRestaurant(data)),
-  setChoosenProduct: (data: Product[]) => dispatch(setChoosenProduct(data)),
+  setRestaurant: (data: Member) => dispatch(setRestaurant(data)),
+  setChoosenProduct: (data: Product) => dispatch(setChoosenProduct(data)),
 });
 const choosenProductRetriever = createSelector(retrieveChoosenProduct, (chosenProduct) => ({
   chosenProduct,
@@ -30,6 +35,24 @@ const restaurantRetriever = createSelector(retrieveRestaurant, (restaurant) => (
 }));
 
 export default function ChosenProduct() {
+  const { productId } = useParams<{productId: string}>();
+  const { setRestaurant, setChoosenProduct } =  actionDispatch(useDispatch());
+  const {chosenProduct} = useSelector(choosenProductRetriever);
+  const {restaurant} = useSelector(restaurantRetriever);
+
+
+  useEffect(() => {
+      const product = new ProductService();
+      product.getProduct(productId)
+        .then((data) => setChoosenProduct(data))
+        .catch((err) => console.log(err));
+
+      const member = new MemberService();
+      member.getRestaurant()
+        .then((data) => setRestaurant(data))
+        .catch((err) => console.log(err));
+  }, []);
+  if(!chosenProduct) return null;
   return (
     <div className={"chosen-product"}>
       <Box className={"title"}>Product Detail</Box>
@@ -42,11 +65,12 @@ export default function ChosenProduct() {
             modules={[FreeMode, Navigation, Thumbs]}
             className="swiper-area"
           >
-            {["/img/cutlet.webp", "/img/kebab-fresh.webp"].map(
+            {chosenProduct?.productImages.map(
               (ele: string, index: number) => {
+                const imagePath = `${serverApi}/${chosenProduct.productImages[0]}`;
                 return (
                   <SwiperSlide key={index}>
-                    <img className="slider-image" src={ele} />
+                    <img className="slider-image" src={imagePath} />
                   </SwiperSlide>
                 );
               }
@@ -55,22 +79,23 @@ export default function ChosenProduct() {
         </Stack>
         <Stack className={"chosen-product-info"}>
           <Box className={"info-box"}>
-            <strong className={"product-name"}>Kebab</strong>
-            <span className={"resto-name"}>Burak</span>
+            <strong className={"product-name"}>{chosenProduct.productName}</strong>
+            <span className={"resto-name"}>{restaurant?.memberNick}</span>
+            <span className={"resto-name"}>{restaurant?.memberPhone}</span>
             <Box className={"rating-box"}>
               <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
               <div className={"evaluation-box"}>
                 <div className={"product-view"}>
                   <RemoveRedEyeIcon sx={{ mr: "10px" }} />
-                  <span>20</span>
+                  <span>{chosenProduct.productViews}</span>
                 </div>
               </div>
             </Box>
-            <p className={"product-desc"}>Our best product</p>
+            <p className={"product-desc"}>{chosenProduct.productDesc ? chosenProduct.productDesc : "No Description" }</p>
             <Divider height="1" width="100%" bg="#000000" />
             <div className={"product-price"}>
               <span>Price:</span>
-              <span>$12</span>
+              <span>${chosenProduct.productPrice}</span>
             </div>
             <div className={"button-box"}>
               <Button variant="contained">Add To Basket</Button>
