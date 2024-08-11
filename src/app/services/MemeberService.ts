640c76bb4b0818fb46b148ddc77c7a6781545ba8
Sam <kimjeyeon03@icloud.com>
2024-08-11 15:52:48 +0900

    fix: create login process

--- src/app/services/MemeberService.ts
+++ src/app/services/MemeberService.ts
@@ -1,6 +1,6 @@
  import axios from "axios";
 import { serverApi } from "../../lib/config";
-import { Member, MemberInput } from "../../lib/types/member";
+import { LoginInput, Member, MemberInput } from "../../lib/types/member";
 class MemberService {
     private readonly path: string;
     constructor() {
@@ -16,7 +16,7 @@ class MemberService {
             console.log("Error getTopUsers:", err);
             throw err;
         }
-    }
+    };
 
     public async getRestaurant(): Promise<Member> {
         try{
@@ -30,7 +30,7 @@ class MemberService {
             console.log("Error getTopUsers:", err);
             throw err;
         } 
-    }
+    };
 
     public async signup(input: MemberInput): Promise<Member> {
         try{
@@ -48,7 +48,25 @@ class MemberService {
             console.log("Error signup:", err);
             throw err;
         }
+    };
+    
+    public async login(input: LoginInput): Promise<Member> {
+        try {
+            const url = this.path + "/member/login";
+            const result = await axios.post(url, input, { withCredentials: true });
+            console.log("login:", result);
+
+            const member: Member = result.data.member;
+            console.log("member:", member);
+            localStorage.setItem("memberData", JSON.stringify(member));
+
+            return member;
+        } catch (err) {
+            console.log("Error login:", err);
+            throw err;
+        }
     }
+
 }
 
     export default MemberService;
